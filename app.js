2f6295caa8e87e881b774082e3339777fa150c02
Jakub Smiarowski <jakub.smiarowski@gmail.com>
2019-05-04 21:15:26 +0200

    Final mobile version, burger animation doesn't work...

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+// funkcja nadania klasy active
 function toggleMenu(visible) {
     document.querySelector('.side-navbar').classList.toggle('active', visible)
-}
 
+}
+// listener na burgera
 document.querySelector('.burger').addEventListener('click', function(event) {
     event.preventDefault();
+    // animacja burgera
+    document.querySelector('.burger').classList.toggle('toggle');
+
     toggleMenu();
 });
