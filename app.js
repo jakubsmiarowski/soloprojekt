09095dc8eb0cef9efae5ab947bebcd60f18224dd
Jakub Smiarowski <jakub.smiarowski@gmail.com>
2019-05-05 14:48:45 +0200

    Add modal v1

--- app.js
+++ app.js
@@ -11,3 +11,37 @@ document.querySelector('.burger').addEventListener('click', function(event) {
 
     toggleMenu();
 });
+
+
+// modal
+
+function closeModal() {
+    document.getElementById('overlay').classList.remove('show')
+};
+
+document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+        e.preventDefault();
+        closeModal();
+    })
+});
+
+document.querySelector('#overlay').addEventListener('click', function(e) {
+    if (e.target === this) {
+        closeModal();
+    }
+});
+
+document.addEventListener('keyup', function(e) {
+    if (e.keyCode === 27) {
+        closeModal();
+    }
+});
+
+function openModal(modal) {
+    document.getElementById('quit').forEach(function(modal) {
+        modal.classList.remove('show');
+    });
+    document.querySelector('#overlay').classList.add('show');
+    document.querySelector(modal).classList.add('show');
+};
